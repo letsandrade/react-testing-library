ed56132b7be9930ebb8e1bbdfff9e0d112e86f97
leticiaandrade <leticia933@gmail.com>
2022-02-18 17:00:18 -0300

    Iniciando o projeto. Vqv!

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+// import { render, screen } from '@testing-library/react';
+// import App from '../App';
+
+describe('requisito 1', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
+    () => {
+
+    });
+});
